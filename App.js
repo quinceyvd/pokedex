946ae4db5284ea897b35307b2e498a07dd33da93
quinceyvd <90757119+quinceyvd@users.noreply.github.com>
2023-11-20 09:57:16 +0100

    init

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Pokédex</Text>
+      <Text>Interactive Pokédex</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 50,
+  }
 });
